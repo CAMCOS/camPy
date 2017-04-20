029b57f64c239a35d2f893c939bd62bea05ad7e5
AndrewZastovnik <Andrew.Zastovnik@gmail.com>
2017-04-20 12:56:19 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='camPy',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/CAMCOS/camPy',
+    license='Proprietary   Do not distribute outside of the Department of Mathematics, San Jose State University',
+    author='Andrew',
+    author_email='',
+    description='A repository for CAMcos PYthon code'
+)
