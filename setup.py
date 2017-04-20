092d40245fe8483a51f4238aab4f1c2b2c427c04
AndrewZastovnik <Andrew.Zastovnik@gmail.com>
2017-04-20 13:06:54 -0700

    added package

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name='camPy',
     version='0.1',
-    packages=[''],
+    packages=['camPy'],
     url='https://github.com/CAMCOS/camPy',
     license='Proprietary   Do not distribute outside of the Department of Mathematics, San Jose State University',
     author='Andrew',
     author_email='',
     description='A repository for CAMcos PYthon code'
 )
+
